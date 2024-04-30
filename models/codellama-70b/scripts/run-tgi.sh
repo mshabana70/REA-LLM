d434f5bfb000b75f17fbcc206e1de8cb3b6157ac
Mahmoud Shabana <mshabana5656@gmail.com>
2024-04-29 20:21:35 -0400

    Update run-tgi.sh

--- models/codellama-70b/scripts/run-tgi.sh
+++ models/codellama-70b/scripts/run-tgi.sh
@@ -9,7 +9,7 @@ token_size=4000
 token_memory=6000
 HOST=0.0.0.0
 PORT=8000
-access_token="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"
+access_token="TOKEN"
 export HUGGING_FACE_HUB_TOKEN=$access_token
 
 #singularity exec --nv $volume text-generation-server download-weights $model
